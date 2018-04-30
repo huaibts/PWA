2ee40f0e4c268addc1c390b609e206228ea250f3
huaijung Huang <je3rjoy@gmail.com>
2018-05-01 00:02:30 +0800

    1
    1

--- sw.js
+++ sw.js
@@ -1,2 +1,26 @@
-¡yuse strict¡z;
-importScripts(¡ysw-toolbox.js¡z); toolbox.precache([¡uindex.html¡v,¡vstyle/style.css¡v]); toolbox.router.get(¡y/images/*¡z, toolbox.cacheFirst); toolbox.router.get(¡y/*¡z, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+const cacheVersion = 'v1';
+const filesToCache = [
+  'index.html',
+  'register_sw.js',
+  
+];
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cacheVersion)
+    .then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+});
+self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] fetch', event.request);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
+});
